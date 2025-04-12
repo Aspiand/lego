b3fc3ed6ead7e5b183e897117bb9fe36ec6efafa
Aspian <muhammad.aspian.d@gmail.com>
2025-04-13 03:30:51 +0800

    hash, save, load

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"io"
 	"log"
 	"os"
 )
@@ -9,15 +13,58 @@ type File struct {
 	Name         string
 	Path         string
 	ModifiedTime string
+	Hash         string
 	Size         int64
 }
 
-func get_files(path string) []File {
-	entries, err := os.ReadDir(path)
+func save(files []File) error {
+	file, err := os.Create("files.json")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	return encoder.Encode(files)
+}
+
+func load() []File {
+	file, err := os.Open("files.json")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var files []File
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&files)
+	if err != nil {
+		panic(err)
+	}
+
+	return files
+}
+
+func sha256sum(path string) string {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	hash := sha256.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		panic(err)
+	}
+
+	return hex.EncodeToString(hash.Sum(nil))
+}
+
+func getFiles(directory string) []File {
+	entries, err := os.ReadDir(directory)
 
 	if err != nil {
-		log.Fatal(err)
-		return nil
+		panic(err)
 	}
 
 	var files []File
@@ -27,18 +74,19 @@ func get_files(path string) []File {
 			continue
 		}
 
-		stat, err := os.Stat(path + "/" + entry.Name())
+		var filePath string = directory + "/" + entry.Name()
+		stat, err := os.Stat(filePath)
 
 		if err != nil {
-			log.Fatal(err)
-			return nil
+			panic(err)
 		}
 
 		files = append(files, File{
 			Name:         stat.Name(),
-			Path:         path + "/" + stat.Name(),
+			Path:         filePath,
 			ModifiedTime: stat.ModTime().String(),
 			Size:         stat.Size(),
+			Hash:         sha256sum(filePath),
 		})
 	}
 
@@ -46,12 +94,17 @@ func get_files(path string) []File {
 }
 
 func main() {
-	var files []File = get_files("/home/ao/Downloads")
+	var files []File = getFiles("/home/ao/Downloads")
+	save(files)
+
+	// var files []File = load()
 
 	for _, file := range files {
 		log.Println(file.Name)
 		log.Println(file.Path)
 		log.Println(file.ModifiedTime)
 		log.Println(file.Size)
+		log.Println(file.Hash)
+		break
 	}
 }
