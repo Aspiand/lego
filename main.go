fc60f49808d872aaee1a1df53760250121c3179e
Aspian <muhammad.aspian.d@gmail.com>
2025-04-13 20:17:58 +0800

    test, log to file, constaint, verify, new file notification, ...

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 type File struct {
@@ -18,8 +19,8 @@ type File struct {
 	Size         int64
 }
 
-func save(files []File) error {
-	file, err := os.Create("files.json")
+func save(filepath string, files []File) error {
+	file, err := os.Create(filepath)
 	if err != nil {
 		panic(err)
 	}
@@ -29,10 +30,10 @@ func save(files []File) error {
 	return encoder.Encode(files)
 }
 
-func load() []File {
-	file, err := os.Open("files.json")
+func load(filepath string) []File {
+	file, err := os.Open(filepath)
 	if err != nil {
-		panic(err)
+		return nil
 	}
 	defer file.Close()
 
@@ -46,11 +47,10 @@ func load() []File {
 	return files
 }
 
-func sha256sum(path string) string {
+func sha256sum(path string) (string, error) {
 	file, err := os.Open(path)
-	if err != nil && os.IsNotExist(err) {
-		log.Fatal("File not found:", path)
-		return ""
+	if os.IsNotExist(err) || os.IsPermission(err) {
+		return "", err
 	}
 	defer file.Close()
 
@@ -59,61 +59,121 @@ func sha256sum(path string) string {
 		panic(err)
 	}
 
-	return hex.EncodeToString(hash.Sum(nil))
+	return hex.EncodeToString(hash.Sum(nil)), nil
 }
 
 func getFiles(directory string) ([]File, error) {
 	var files []File
 
-	e := filepath.Walk(directory, func(path string, info os.FileInfo, err error) error {
+	err := filepath.Walk(directory, func(path string, info os.FileInfo, err error) error {
 		if info.IsDir() {
 			return nil
 		}
 
+		hash, _ := sha256sum(path)
 		files = append(files, File{
 			Name:         info.Name(),
 			Path:         path,
 			ModifiedTime: info.ModTime().String(),
 			Size:         info.Size(),
-			Hash:         sha256sum(path),
+			Hash:         hash,
 		})
 
 		return nil
 	})
 
-	if e != nil {
-		panic(e)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	return files, nil
 }
 
+func constaint(files []File, file File) bool {
+	for _, item := range files {
+		if item.Name == file.Name && item.Path == file.Path && item.ModifiedTime == file.ModifiedTime && item.Size == file.Size && item.Hash == file.Hash {
+			return true
+		}
+	}
+
+	return false
+}
+
+func verify(oldFiles []File, newFiles []File) (bool, []File) {
+	// not efficient, but works
+
+	var newVerifiedData []File
+
+	for _, file := range newFiles {
+		if constaint(oldFiles, file) {
+			continue
+		}
+
+		newVerifiedData = append(newVerifiedData, file)
+		log.Println("New file found:", file.Path)
+	}
+
+	if len(newVerifiedData) != 0 {
+		return true, newVerifiedData
+	}
+
+	return false, nil
+}
+
 func main() {
-	path := "/home/ao/Downloads"
-	// path = "/nix/store"
-	files, err := getFiles(path)
+	const directory string = "/home/ao/Downloads"
+	const savePath string = "files.json"
+	logFile, _ := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666) // err later
+	defer logFile.Close()
 
-	if err != nil {
-		log.Fatal(err)
+	multiWriter := io.MultiWriter(os.Stdout, logFile)
+	// log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.SetOutput(multiWriter)
+
+	verifiedData := load(savePath)
+	if verifiedData == nil {
+		log.Println("No verified data found, creating new file.")
+		verifiedData, _ = getFiles(directory)
+		save(savePath, verifiedData)
+	} else {
+		log.Println("Verified data found, loading existing file.")
 	}
 
-	// return
+	// fmt.Println(verifiedData)
 
-	// save(files)
-	// // var files []File = load()
+	// for _, file := range verifiedData {
+	// 	log.Println(file.Name)
+	// 	log.Println(file.Path)
+	// 	log.Println(file.ModifiedTime)
+	// 	log.Println(file.Size)
+	// 	log.Println(file.Hash)
 
-	for _, file := range files {
-		log.Println(file.Name)
-		log.Println(file.Path)
-		log.Println(file.ModifiedTime)
-		log.Println(file.Size)
-		log.Println(file.Hash)
-
-		// break
-	}
-
-	// for {
-	// 	time.Sleep(1 * time.Second)
-	// 	log.Println("Checking for changes...")
+	// 	fmt.Println("-----------------------------------")
+	// 	// break
 	// }
+
+	for {
+		time.Sleep(10 * time.Second)
+		log.Println("Checking for changes...")
+		files, err := getFiles(directory)
+		if err != nil {
+			log.Println("Error:", err)
+			continue
+		}
+
+		// Check files.json exists
+		if _, err := os.Stat(savePath); os.IsNotExist(err) {
+			log.Println("files.json not found, creating new file.")
+			save(savePath, files)
+			continue
+		}
+
+		status, newFile := verify(verifiedData, files)
+		if status {
+			verifiedData = append(verifiedData, newFile...)
+			save(savePath, verifiedData)
+		} else {
+			log.Println("No changes found.")
+		}
+	}
 }
