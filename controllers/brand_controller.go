a21e636538503f892a2a435ba8f4997a9ad93f17
Aspian <muhammad.aspian.d@gmail.com>
2025-04-24 07:00:57 +0800

    feat: add GetByID and Update methods to BrandController; update routes accordingly

--- controllers/brand_controller.go
+++ controllers/brand_controller.go
@@ -27,6 +27,17 @@ func (ctl BrandController) GetAll(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, brands)
 }
 
+func (ctl BrandController) GetByID(c *gin.Context) {
+	var brand models.Brand
+
+	if err := ctl.DB.First(&brand, c.Param("id")).Error; err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"error": "ID not found"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, brand)
+}
+
 func (ctl BrandController) Create(c *gin.Context) {
 	var brand models.Brand
 
@@ -43,7 +54,20 @@ func (ctl BrandController) Create(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, brand)
 }
 
-func (ctl BrandController) Delete(c *gin.Context) { // later
+func (ctl BrandController) Update(c *gin.Context) {
+	var brand models.Brand
+	id := c.Param("id")
+
+	if err := c.ShouldBindJSON(&brand); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctl.DB.Model(&models.Brand{}).Where("id = ?", id).Updates(brand)
+	c.Status(http.StatusOK)
+}
+
+func (ctl BrandController) Delete(c *gin.Context) {
 	id := c.Param("id")
 
 	if err := ctl.DB.Delete(&models.Brand{}, id).Error; err != nil {
@@ -51,5 +75,15 @@ func (ctl BrandController) Delete(c *gin.Context) { // later
 		return
 	}
 
-	c.Status(http.StatusNoContent)
+	c.Status(http.StatusOK)
+}
+
+func (ctl BrandController) DeleteAll(c *gin.Context) {
+	result := ctl.DB.Where("1 == 1").Delete(&models.Brand{})
+
+	if result.Error != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+	}
+
+	c.IndentedJSON(http.StatusOK, result.RowsAffected)
 }
--- routers/brand.go
+++ routers/brand.go
@@ -10,8 +10,11 @@ func registerBrandRoutes(db *gorm.DB, rg *gin.Engine) {
 	brand := rg.Group("/brands")
 	controller := controllers.NewBrandController(db)
 	{
-		brand.GET("/", controller.GetAll)
-		brand.POST("/", controller.Create)
+		brand.GET("", controller.GetAll)
+		brand.POST("", controller.Create)
+		brand.DELETE("", controller.DeleteAll)
+		brand.GET("/:id", controller.GetByID)
+		brand.PUT("/:id", controller.Update)
 		brand.DELETE("/:id", controller.Delete)
 	}
 }
